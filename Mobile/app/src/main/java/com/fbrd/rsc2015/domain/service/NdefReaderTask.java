6c9acf0824e6d96a03d5dc4e301b682f7e7c342d
skliba <kliba.stefano@gmail.com>
2015-11-21 23:54:42 +0100

    nfc finished

---
+++ Mobile/app/src/main/java/com/fbrd/rsc2015/domain/service/NdefReaderTask.java
@@ -0,0 +1,84 @@
+package com.fbrd.rsc2015.domain.service;
+
+import android.content.Context;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+/**
+ * Created by noxqs on 21.11.15..
+ */
+public class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+
+    private Context context;
+
+    public NdefReaderTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Tag... params) {
+        Tag tag = params[0];
+
+        Ndef ndef = Ndef.get(tag);
+        if (ndef == null) {
+            return null;
+        }
+
+        NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+        NdefRecord[] records = ndefMessage.getRecords();
+        for (NdefRecord ndefRecord : records) {
+            if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                try {
+                    return readText(ndefRecord);
+                } catch (UnsupportedEncodingException e) {
+                    Log.e("FAILEDENCODING", "Unsupported Encoding", e);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private String readText(NdefRecord record) throws UnsupportedEncodingException {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+        byte[] payload = record.getPayload();
+
+        // Get the Text Encoding
+        String textEncoding = "UTF-8";
+
+        // Get the Language Code
+        int languageCodeLength = payload[0] & 51;
+
+        //String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+        // e.g. "en"
+
+        // Get the Text
+        return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if (result != null) {
+            Toast.makeText(context, "Result: " + result, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
--- Mobile/app/src/main/java/com/fbrd/rsc2015/ui/activity/NFCScanActivity.java
+++ Mobile/app/src/main/java/com/fbrd/rsc2015/ui/activity/NFCScanActivity.java
@@ -1,13 +1,18 @@
 package com.fbrd.rsc2015.ui.activity;
 
 import com.fbrd.rsc2015.R;
+import com.fbrd.rsc2015.domain.service.NdefReaderTask;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -51,7 +56,32 @@ public class NFCScanActivity extends AppCompatActivity {
     }
 
     private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
 
+            String type = intent.getType();
+            if (MIME_TEXT_PLAIN.equals(type)) {
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask(this).execute(tag);
+
+            } else {
+                Log.d(TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                    new NdefReaderTask(this).execute(tag);
+                    break;
+                }
+            }
+        }
     }
 
     @Override
@@ -62,6 +92,24 @@ public class NFCScanActivity extends AppCompatActivity {
 
     private void setupForegroundDispatch(NFCScanActivity nfcScanActivity, NfcAdapter adapter) {
         final Intent intent = new Intent(nfcScanActivity.getApplicationContext(), nfcScanActivity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(nfcScanActivity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(nfcScanActivity, pendingIntent, filters, techList);
     }
 
     @Override
@@ -79,4 +127,5 @@ public class NFCScanActivity extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         handleIntent(intent);
     }
+
 }
