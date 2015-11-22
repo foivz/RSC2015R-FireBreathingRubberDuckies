b715f23e9e6ebe7fa26fcdd8f959c843a86f2a0f
skliba <kliba.stefano@gmail.com>
2015-11-22 02:33:16 +0100

    date parser

---
+++ Mobile/app/src/main/java/com/fbrd/rsc2015/domain/util/DateTimeHelper.java
@@ -0,0 +1,56 @@
+package com.fbrd.rsc2015.domain.util;
+
+import org.joda.time.DateTime;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+import org.joda.time.Seconds;
+
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by noxqs on 22.11.15..
+ */
+public class DateTimeHelper {
+
+    private DateTime startedAt;
+
+    private DateTime endedAt;
+
+    private String finalTime = "";
+    private int duration;
+
+    public DateTimeHelper(DateTime startedAt, DateTime endedAt, int duration) {
+        this.startedAt = startedAt;
+        this.endedAt = endedAt;
+        this.duration = duration;
+        calculateElapsedTime();
+    }
+
+    private void calculateElapsedTime() {
+        int hours, minutes, seconds;
+
+        if(duration > 60){
+            startedAt = startedAt.plusMinutes(duration);
+        }
+
+        SimpleDateFormat now = new SimpleDateFormat("HH:mm:ss");
+        Date currDate = new Date();
+
+        hours = Hours.hoursBetween(new DateTime(startedAt), new DateTime(currDate)).getHours();
+        finalTime = "" + hours;
+        minutes = Minutes.minutesBetween(new DateTime(startedAt), new DateTime(currDate)).getMinutes();
+        finalTime = ":" + minutes;
+        seconds = Seconds.secondsBetween(new DateTime(startedAt), new DateTime(currDate)).getSeconds();
+        finalTime = ":" + seconds;
+
+        Log.e("FinalTime", finalTime);
+
+    }
+}
+
+
+
